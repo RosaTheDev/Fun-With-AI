5e7d5b7006a16c5667abe66a747f139f114de5a1
Richard Rosa-Serrano <richardjrosaserrano@gmail.com>
2022-05-21 19:35:47 -0400

    Created the component that will hold the props that contain the prompt and response, I have it displaying in an h1 for readablility, and will need to style this component

---
+++ pages/Response Container/ResponseInfoCards/ResponseInfoCards.js
@@ -0,0 +1,9 @@
+const ResponseInfoCards = ({prompt, response}) => {
+  return (
+    <div>
+      <h1>Prompt: {prompt} <br/><br/> Response: {response}</h1>
+    </div>
+  )
+}
+
+export default ResponseInfoCards;
