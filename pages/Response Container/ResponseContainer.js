6147b2527c075a1614c95f7427dd9498eda3df92
Richard Rosa-Serrano <richardjrosaserrano@gmail.com>
2022-05-21 19:19:54 -0400

    Created the Response Container component, and I will use this to create the caards with the prompts and the responses that the AI gives

---
+++ pages/Response Container/ResponseContainer.js
@@ -0,0 +1,15 @@
+import {useState} from "react";
+
+const ResponseContainer = ({prompt, response}) => {
+  console.log(prompt)
+  console.log(response)
+  return(
+    <div>
+      <h1>I am a card</h1>
+      <h1>I am a card</h1>
+      <h1>I am a card</h1>
+    </div>
+  )
+}
+
+export default ResponseContainer;
