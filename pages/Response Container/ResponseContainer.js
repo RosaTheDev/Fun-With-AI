752756b3294b7cd1bc327e9f7fe7e73d6a8f077e
Richard Rosa-Serrano <richardjrosaserrano@gmail.com>
2022-05-21 19:28:56 -0400

    Created state for the Prompt to be stored in an array, that array was also placing an empty object into the first index, to solve this I filtered the array to remove the empty object, the new array is then stored in the variable results

--- pages/Response Container/ResponseContainer.js
+++ pages/Response Container/ResponseContainer.js
@@ -1,8 +1,24 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 const ResponseContainer = ({prompt, response}) => {
-  console.log(prompt)
-  console.log(response)
+  const[promptArray, setPromptArray] = useState([]);
+  
+  useEffect(() => {
+    if(!promptArray.includes(prompt)) {
+      setPromptArray([...promptArray, prompt]);
+    } else {
+      return prompt
+    }
+  })
+  
+ const results = promptArray.filter(element => {
+    if(Object.keys(element).length !== 0) {
+      return true
+    }
+    return false
+  })
+
+  console.log(results)
   return(
     <div>
       <h1>I am a card</h1>
